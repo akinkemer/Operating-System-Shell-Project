ae12041fb59a1f8ebb764ff7e8152867468d2c3e
akinkemer <kemerakin@gmail.com>
2020-12-15 14:05:40 +0300

    strtok

--- src/shell.c
+++ src/shell.c
@@ -6,78 +6,97 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-void parse(char *line, char **argv)
-{
-  int in = 0;      /* simple in-word flag 0-false/1-true */
-  size_t n = 0;    /* counter to protect splitInput bounds */
-  char *sp = line; /* start-pointer initialized to userInput */
+#define MAXCMD 64
 
-  while (n < 64 - 1)
-  { /* loop while pointers remain unfilled */
-    /* if at end, is whitespace or a delimiter */
-    if (!*line || isspace(*line) || *line == ';')
-    {
-      if (in)
-      {                 /* if in word */
-        argv[n++] = sp; /* set pointer to start-pointer */
-        argv[n] = NULL; /* set next pointer NULL */
-      }
-      in = 0;    /* reset in flag zero */
-      if (*line) /* if space or delim, nul-terminate */
-        *line = 0;
-      else      /* otherwise */
-        return; /* at end-of-string */
-    }
-    else
-    { /* normal char */
-      if (!in)
-      {            /* if not in-word */
-        sp = line; /* set start-pointer to 1st good char */
-        in = 1;    /* set in-word flag true */
-      }
-    }
-    line++; /* advance to next char */
+void parseToParameters(char *line, char **argv)
+{
+  const char s[2] = " ";
+  int i = 0;
+  *argv = NULL;
+  char *token;
+
+  token = strtok(line, s);
+
+  while (token != NULL)
+  {
+    argv[i] = token;
+    token = strtok(NULL, s);
+    i++;
+  }
+  for(i;i<3;i++){
+    argv[i]=NULL;
+  }
+}
+void parseToCommands(char *line, char **argv)
+{
+  const char s[2] = ";";
+  int i = 0;
+  *argv = NULL;
+  char *token;
+
+  token = strtok(line, s);
+
+  while (token != NULL)
+  {
+    argv[i] = token;
+    token = strtok(NULL, s);
+    i++;
+  }
+  for (i; i < MAXCMD; i++)
+  {
+    argv[i] = NULL;
   }
 }
 
-  void execute(char **argv)
-  {
-    pid_t pid;
-    int status;
+void execute(char **argv)
+{
+  pid_t pid;
+  int status;
 
-    if ((pid = fork()) < 0)
-    { /* fork a child process           */
-      printf("*** ERROR: forking child process failed\n");
+  if ((pid = fork()) < 0)
+  { /* fork a child process           */
+    printf("*** ERROR: forking child process failed\n");
+    exit(1);
+  }
+  else if (pid == 0)
+  { /* for the child process:         */
+    if (execvp(*argv, argv) < 0)
+    { /* execute the command  */
+      printf("*** ERROR: exec failed\n");
       exit(1);
     }
-    else if (pid == 0)
-    { /* for the child process:         */
-      if (execvp(*argv, argv) < 0)
-      { /* execute the command  */
-        printf("*** ERROR: exec failed\n");
-        exit(1);
+  }
+  else
+  {                              /* for the parent:      */
+    while (wait(&status) != pid) /* wait for completion  */
+      ;
+  }
+}
+
+void main(void)
+{
+  char line[512];
+  char *argvCommands[MAXCMD];
+  char *argvParameters[3];
+
+  while (1)
+  {
+    printf("prompt> ");
+    fgets(line, 512, stdin);
+    printf("\n");
+    parseToCommands(line, argvCommands);
+    if (strcmp(argvCommands[0], "quit") == 0)
+    {
+      exit(0);
+    }
+
+    int i;
+    for(i=0;i<MAXCMD;i++){
+      if(argvCommands[i]!=NULL){
+        parseToParameters(argvCommands[i],argvParameters);
+        execute(argvParameters);
       }
     }
-    else
-    {                              /* for the parent:      */
-      while (wait(&status) != pid) /* wait for completion  */
-        ;
-    }
+    
   }
-
-  void main(void)
-  {
-    char line[512]; /* the input line                 */
-    char *argv[64]; /* the command line argument      */
-
-    while (1)
-    {                          /* repeat until done ....         */
-      printf("prompt> ");      /*   display a prompt             */
-      fgets(line, 512, stdin); /*   read in the command line     */
-      printf("\n");
-      parse(line, argv);                /*   parse the line               */
-      if (strcmp(argv[0], "quit") == 0) /* is it an "exit"?     */
-        exit(0);                        /*   exit if it is                */
-      execute(argv);                    /* otherwise, execute the command */
-    }
-  }
+}
